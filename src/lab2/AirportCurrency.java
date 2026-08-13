a321b9483f7a99f868d4e4790c901457b4d610a0
adelerichardsonsmith-pixel <adelerichardsonsmith@gmail.com>
2026-08-13 15:17:53 +0100

    lab2 airport currency code

---
+++ src/lab2/AirportCurrency.java
@@ -0,0 +1,79 @@
+package lab2;
+
+public class AirportCurrency {
+
+	public static void main(String[] args) {
+
+		//values for input
+		double gbpAmount = 100.0;
+		int currencyChoice = 1;
+		char customerType = 'S';
+		
+		//determine currency rate
+		float exchangeRate = 0.0f;
+		String currencyName = "";
+		
+		switch (currencyChoice) {
+		case 1:
+			exchangeRate = 1.25f;
+			currencyName = "USD";
+			break;
+		case 2:
+			exchangeRate = 1.15f;
+			currencyName = "EUR";
+			break;
+		case 3:
+			exchangeRate = 180.5f;
+			currencyName = "JPY";
+			break;
+		case 4:
+			exchangeRate = 0.68f;
+			currencyName = "AUD";
+			break;
+		default:
+			System.out.println("Error: Invalid currency choice.");
+			break;
+			
+		}
+		
+		//calculate fee
+		double feePercentage;
+        if (gbpAmount < 100) {
+            feePercentage = 0.05;
+        } else {
+            feePercentage = 0.025;
+        }
+        double amountAfterFee = gbpAmount - (gbpAmount * feePercentage);
+        
+        //calculate tax
+        double taxPercentage = 0.0;
+
+        switch (customerType) {
+            case 'V':
+                taxPercentage = 0.05;
+                break;
+                
+            case 'R':
+                taxPercentage = 0.10;
+                break;
+            case 'S':
+            	taxPercentage = 0.0;
+            	break;
+            default:
+                System.out.println("Error: Invalid customer type.");
+                break;
+        }
+
+        double amountAfterTax = amountAfterFee - (amountAfterFee * taxPercentage);
+
+        double rawForeignCurrency = amountAfterTax * exchangeRate;
+        int finalForeignCurrency = (int) rawForeignCurrency;
+        
+        System.out.println("--- TRANSACTION SUMMARY ---");
+        System.out.println("Initial Amount: £" + gbpAmount);
+        System.out.println("Amount after Service Fee: £" + amountAfterFee);
+        System.out.println("Amount after Airport Tax: £" + amountAfterTax);
+        System.out.println("Final Amount in Foreign Currency: " +currencyName  + "" + finalForeignCurrency);
+	}
+
+}
---
+++ src/lab2/Program.java
@@ -0,0 +1,31 @@
+package lab2;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		int grade = -11;
+		
+		if (grade < 1 || grade > 100) {
+			System.out.println("Error: marks must be between 1 and 100");
+		}
+		else {
+		
+		if ( grade > 71) {
+			System.out.println("Dinstinction");
+		}
+		else if ( grade > 61) {
+			System.out.println("Merit");
+		}
+		else if ( grade > 50) {
+			System.out.println("Pass");
+		}
+		else {
+			System.out.println("Fail");
+		}
+		}
+		
+
+	}
+
+}
