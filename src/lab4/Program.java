5c2bb2bf5d1520f09cd7e7d68f1b7d271dd971fa
adelerichardsonsmith-pixel <adelerichardsonsmith@gmail.com>
2026-08-14 15:15:28 +0100

    lab4amends

--- src/lab4/Program.java
+++ src/lab4/Program.java
@@ -6,59 +6,39 @@ public class Program {
 		
 		int[] numbers = {1, 3, -5, 7, 0, 4, 6, 8 };
 		printArray(numbers);
-		
-		//Task1
 		int sum = 0;
+	    int average = (int) sum / numbers.length;
+        int min = numbers[0];
+        int max = numbers[0];
+        int zero = -1;
+        
 		for (int num : numbers) {
 		    sum += num;
 		}
-
-		System.out.println("Sum:" + sum);
-	    
-	    //Task2
-	    int average = (int) sum / numbers.length;
-        System.out.println("Average:" + average);
-        
-        //Task3
-        int min = numbers[0];
-
         for (int num : numbers) {
             if (num < min) {
                 min = num;
             }
         }
-        
-        System.out.println("Minimum:" + min);
-
-        
-        //Task 4
-        int max = numbers[0];
-        
         for (int num : numbers ) {
         	if (num > max) {
         		max = num;
         	}
-        	
         }
-        System.out.println("Maximum:" +max);
-        
-        //Task 5
-        int zero = -1;
-
         for (int i = 0; i < numbers.length; i++) {
             if (numbers[i] == 0) {
                 zero = i;
-                break; // Exit early once zero is found
+                break;
             }
         }
-
-        System.out.println("Index of 0:" + zero);
         
+		System.out.println("Sum:" + sum);
+        System.out.println("Average:" + average);
+        System.out.println("Minimum:" + min);
+        System.out.println("Maximum:" +max);
+        System.out.println("Index of 0:" + zero);
 	}
 	
 	public static void printArray(int[] arr) {
-		
 	}
-		
-
 }
