91283cd3e6333b41821033fa1273a4bbbb1421e5
adelerichardsonsmith-pixel <adelerichardsonsmith-pixel@githubl.com>
2026-08-18 15:33:43 +0100

    race

--- src/lab3b/Program.java
+++ src/lab3b/Program.java
@@ -9,9 +9,6 @@ public class Program {
 		
 		ArrayList<Vehicle> vehicles = new ArrayList<>();
 		
-		Random rand = new Random();
-		boolean raceFinished = false;
-		
 		vehicles.add(new Vehicle(1));
 		vehicles.add(new Vehicle(2));
 		vehicles.add(new Vehicle(3));
@@ -21,18 +18,33 @@ public class Program {
 		
 		for (Vehicle v : vehicles) {
 			System.out.println(v.getDetails());
-		}
+		}	
+		
+		raceVehicles(vehicles);
+	} 
+	
+	public static void raceVehicles(ArrayList<Vehicle> vehicles) {
+		boolean raceFinished = false;
+		Random rand = new Random();
+		int turn = 1;
 		
 		while (!raceFinished) {
+			
 			for (Vehicle v : vehicles) {
 				int n = rand.nextInt(10)+1;
 				v.accelerate(n);
+				System.out.println(v.getDetails());
+
 				if (v.distanceTravelled > 1000) {
+					System.out.println("Vehicle: " + v.registrationPlate.getNumber() + "wins, distance travelled: " + v.distanceTravelled);
 					raceFinished = true;
+					break;
 				}
 			}
-			
+			turn ++;
+
 		}
-			
+		
+
 	}
 }
